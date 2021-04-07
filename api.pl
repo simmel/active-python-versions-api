e457441bf53f9309dcd952a0f2fa2ca1b1d13c7c
Simon Lundström <github-commits@soy.se>
2021-04-07 23:12:29 +0200

    Add parse_versions helper

--- api.pl
+++ api.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite -signatures;
 
+app->helper(parse_versions => sub ($c, @values) {
+  my $html = $values[0];
+  return $html;
+});
+
 under '/v1';
 
 get '/' => {text => "I 🐍 Python!\n"};
