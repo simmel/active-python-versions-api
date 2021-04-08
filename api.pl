df0dd5c10aeab5aaed79ecd0c7fe32b662360de6
Simon Lundström <github-commits@soy.se>
2021-04-08 08:43:17 +0200

    Update Python versions every day

--- api.pl
+++ api.pl
@@ -8,6 +8,40 @@ app->helper(parse_versions => sub ($c, @values) {
   return $data;
 });
 
+my $VERSION = "1.0.0";
+my $data = [];
+
+Mojo::IOLoop->recurring(86400 => sub ($ioloop) {
+    update_python_versions();
+});
+
+sub update_python_versions() {
+  app->log->debug("Starting to load Python versions");
+  app->ua->transactor->name("active-python-versions-api/$VERSION (+https://github.com/simmel/active-python-versions-api)");
+  app->ua->connect_timeout(10)->request_timeout(10)->get('https://www.python.org/downloads/' => sub ($ua, $tx) {
+    my $html;
+    my $result = $tx->result;
+    if ($result->is_success) {
+      $html = $result->body;
+      $data = app->parse_versions($html);
+      my $versions = scalar(@$data);
+      if ($versions == 0) {
+        app->log->error("Loaded no Python versions");
+      }
+      else {
+        app->log->debug("Loaded python versions: ".join(",", @$data));
+        app->log->info("Loaded ".scalar(@$data)." Python versions");
+      }
+    }
+    else {
+      app->log->error("Couldn't load Python versions: ". $result->message);
+      return;
+    }
+  });
+}
+
+update_python_versions();
+
 under '/v1';
 
 get '/' => {text => "I 🐍 Python!\n"};
