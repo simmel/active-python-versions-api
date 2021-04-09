7fd676e43e5347d0a80a98f925d71d81b909b2fd
Simon Lundström <github-commits@soy.se>
2021-04-09 13:45:43 +0200

    We can't modify $data if we're async
    Weird but I don't have time to troubleshoot

--- api.pl
+++ api.pl
@@ -23,26 +23,25 @@ Mojo::IOLoop->recurring(86400 => sub ($ioloop) {
 sub update_python_versions() {
   app->log->debug("Starting to load Python versions");
   app->ua->transactor->name("active-python-versions-api/$VERSION (+https://github.com/simmel/active-python-versions-api)");
-  app->ua->connect_timeout(10)->request_timeout(10)->get('https://www.python.org/downloads/' => sub ($ua, $tx) {
-    my $html;
-    my $result = $tx->result;
-    if ($result->is_success) {
-      $html = $result->body;
-      $data = app->parse_versions($html);
-      my $versions = scalar(@$data);
-      if ($versions == 0) {
-        app->log->error("Loaded no Python versions");
-      }
-      else {
-        app->log->debug("Loaded python versions: ".join(",", @$data));
-        app->log->info("Loaded ".scalar(@$data)." Python versions");
-      }
+  my $result = app->ua->connect_timeout(10)->request_timeout(10)->get('https://www.python.org/downloads/')->result;
+  my $html;
+  if ($result->is_success) {
+    $html = $result->body;
+    $data = app->parse_versions($html);
+    app->log->debug("after parse_versions ".join(",", @$data));
+    my $versions = scalar(@$data);
+    if ($versions == 0) {
+      app->log->error("Loaded no Python versions");
     }
     else {
-      app->log->error("Couldn't load Python versions: ". $result->message);
-      return;
+      app->log->debug("Loaded python versions: ".join(",", @$data));
+      app->log->info("Loaded ".scalar(@$data)." Python versions");
     }
-  });
+  }
+  else {
+    app->log->error("Couldn't load Python versions: ". $result->message);
+    return;
+  }
 }
 
 update_python_versions();
