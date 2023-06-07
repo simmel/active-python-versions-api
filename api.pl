27a68ca03250974970dcf4b80b7a49ee39f3fab2
Simon Lundström <github-commits@soy.se>
2023-06-07 19:20:59 +0200

    2.X is gone but prereleases is here
    So just show bugfix and security releases.

--- api.pl
+++ api.pl
@@ -5,7 +5,7 @@ use Mojolicious::Lite -signatures;
 app->helper(parse_versions => sub ($c, @values) {
   my $html = $values[0];
   my $dom = Mojo::DOM->new($html);
-  my $data = $dom->find('ol * span.release-version')->map('text')->grep(qr/^[^2]/)->to_array;
+  my $data = $dom->find('div.active-release-list-widget > ol li span.release-status:is(:text(security), :text(bugfix))')->map("parent")->map("find", ".release-version")->map("first")->map("text")->to_array;
   return $data;
 });
 
